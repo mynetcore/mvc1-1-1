2a7098a4d9be2cf9ebfd9105c33feab763608bd1
Tho Ho <tho@4xlabs.co>
2017-02-03 00:02:28 +0800

    Implement echo webApi support get email in route and in query string

---
+++ src/Mvc111/Controllers/EchoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mvc111.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    public class EchoController : Controller
+    {
+        [HttpGet("{email}")]
+        public string GetInRoute(string email)
+        {
+            return $"GetInRoute: [{email}]";
+        }
+
+        [HttpGet]
+        public string GetInQuery(string email)
+        {
+            return $"GetInQuery: [{email}]";
+        }
+    }
+}
